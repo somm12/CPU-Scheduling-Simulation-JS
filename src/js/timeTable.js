fe3c5bc9d1d3dc992f24934f0de85e6f6847ced1
somm12 <sominan8744@gmail.com>
2024-04-29 18:49:29 +0900

    feat: time table 추가 수정

--- src/js/timeTable.js
+++ src/js/timeTable.js
@@ -1,20 +1,44 @@
 const addButton = document.querySelector(".addBtn");
 const removeButton = document.querySelector(".removeBtn");
+const submitButton = document.querySelector(".submitBtn");
+const arrivedTime = document.querySelector(".arrived-time");
+const runningTime = document.querySelector(".running-time");
 
-addButton.addEventListener("click", addRow);
+let timeTable = [];
+const TIMETABLE = "TIMETABLE";
+addButton.addEventListener("click", (event) => {
+  if (timeTable.length >= 5) {
+    alert("5개 까지만 입력해주세요!"); // 5개 까지만 입력 제한
+    event.preventDefault();
+  }
+  if (arrivedTime.value && runningTime.value) addRow();
+});
+submitButton.addEventListener("click", submitTable);
+
+function submitTable() {
+  // 다음 화면으로 넘어가자.
+}
+
+function saveTimeTable() {
+  localStorage.setItem(TIMETABLE, JSON.stringify(timeTable));
+}
 
 function removeRow(event) {
   const row = event.target.parentElement.parentElement;
 
   row.parentNode.removeChild(row);
+  console.log(row);
+  timeTable = timeTable.filter((process) => process.id !== parseInt(row.id));
+  saveTimeTable();
 }
 function addRow() {
   // Get the table body element in which you want to add row
+
   let table = document.querySelector(".tableBody");
 
   // Create row element
   let row = document.createElement("tr");
-  row.id = Date.now();
+  row.id = timeTable.length + 1;
 
   // Create cells
   let arrivedTimeColumn = document.createElement("td");
@@ -23,7 +47,9 @@ function addRow() {
 
   // Insert data to cells
   let arrivedTimeInput = document.createElement("input");
+  arrivedTimeInput.value = arrivedTime.value;
   let runningTimeInput = document.createElement("input");
+  runningTimeInput.value = runningTime.value;
   let removeBtn = document.createElement("button");
 
   removeBtn.innerText = "-";
@@ -41,4 +67,23 @@ function addRow() {
   // Append row to table body
 
   table.appendChild(row);
+
+  // timeTable 배열에 추가하기
+  const newId = timeTable.length + 1;
+  const newProcess = {
+    id: newId,
+    at: arrivedTime.value,
+    rt: runningTime.value,
+  };
+  timeTable.push(newProcess);
+  saveTimeTable();
+
+  arrivedTime.value = "";
+  runningTime.value = "";
 }
+
+// function init() {
+//   localStorage.clear();
+// }
+
+// init();
